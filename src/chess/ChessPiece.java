41dab7b7cb46d7c057ca11743ce713f42e9bba7b
mayconb2 <mayconb2@gmail.com>
2019-08-18 20:32:10 -0300

    Added chess layer

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,24 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece{
+	
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	private Color color;
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	
+}
